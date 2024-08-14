5186e2b92e9471bb004fcb3e436f76b9657bac3c
gil <gilson1212@yahoo.com.br>
2024-08-14 11:09:20 -0300

    Controle de Fluxo para o Desafio de Projeto da DIO

---
+++ DesafioControleFluxo/src/controle/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,14 @@
+package controle.exceptions;
+
+public class ParametrosInvalidosException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String mensagem) {
+		super(mensagem);
+	}
+	
+}
